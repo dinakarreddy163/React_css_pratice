5f38c176ee1effd3e4909068d5ddd8ac44b6a0ce
Gopavaram <dinakar-reddy.gopavaram@capgemini.com>
2024-11-21 20:26:28 +0530

    'Changes'

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+          <div className='left'></div>
+          <div className='right'>
+            <div className='right-1'></div>
+            <div className='right-2'></div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
